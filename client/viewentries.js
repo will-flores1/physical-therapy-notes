9c12366fe13ce714ad03c7938f7ca21f8164cecc
wilfredo-flores <willflores1221@gmail.com>
2023-09-22 11:10:13 -0400

    Add: delete button

--- client/viewentries.js
+++ client/viewentries.js
@@ -12,13 +12,42 @@ function createEntryCard(entry) {
 	title.textContent =
 		"Entry Date: " + new Date(entry.createdAt).toLocaleDateString();
 
+	// Attach entry id to the card using data-id attribute
+	card.setAttribute("data-id", entry._id);
+
 	const challengesAndAchievements = document.createElement("p");
 	challengesAndAchievements.textContent =
 		"Challenges and Achievements: " + entry.challengesAndAchievements;
 
+	const deleteEntry = document.createElement("button");
+	deleteEntry.textContent = "Delete";
+	deleteEntry.classList.add("delete-entry-button");
+	deleteEntry.addEventListener("click", async () => {
+		try {
+			const res = await fetch(
+				`http://localhost:3000/api/entries/${entry._id}`,
+				{
+					method: "DELETE",
+				}
+			);
+			const data = await res.json();
+			console.log(data);
+			window.location.reload();
+		} catch (error) {
+			console.error(error);
+		}
+	});
+
+	const editEntry = document.createElement("a");
+	editEntry.textContent = "Edit";
+	editEntry.classList.add("edit-entry-button");
+	editEntry.setAttribute("href", `/entry/edit/${entry._id}`);
+
 	// Append content to the card
 	card.appendChild(title);
 	card.appendChild(challengesAndAchievements);
+	card.appendChild(deleteEntry);
+	card.appendChild(editEntry);
 
 	return card;
 }
@@ -27,12 +56,24 @@ const fetchEntries = (async () => {
 	try {
 		const res = await fetch("http://localhost:3000/api/entries");
 		const entries = await res.json();
+		// sort entries by date
+		entries.sort((a, b) => {
+			if (a.createdAt < b.createdAt) return 1;
+			if (a.createdAt > b.createdAt) return -1;
+			return 0;
+		});
 		console.log(entries);
 
 		const app = $("#app");
 		entries.forEach((entry, idx) => {
 			const entryCard = createEntryCard(entry);
 			app.appendChild(entryCard);
+			entryCard.addEventListener("click", () => {
+				console.log(entryCard.getAttribute("data-id"));
+				window.location.href = `/entry/${entryCard.getAttribute(
+					"data-id"
+				)}`;
+			});
 		});
 	} catch (error) {
 		console.error("Error fetching data:", error);
